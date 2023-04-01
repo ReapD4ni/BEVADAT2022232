82d3a0df89faa72b3fac2c73aff66e3452e3d9fc
Kardos.Dani <kardos.dani.03@gmail.com>
2023-04-01 14:12:44 +0200

    [INIT]

--- HAZI/HAZI05/HAZI05.py
+++ HAZI/HAZI05/HAZI05.py
@@ -6,7 +6,7 @@ from sklearn.metrics import confusion_matrix
 import math
 
 
-#cs_path="diabetes.csv"
+csv_path="diabetes.csv"
 
 class KNNClassifier:
 
@@ -14,6 +14,11 @@ class KNNClassifier:
     def __init__(self,k:int,test_split_ratio:float):
         self.k = k
         self.test_split_ratio = test_split_ratio
+        self.x_train = None
+        self.y_train = None
+        self.x_test = None
+        self.y_test = None
+        self.y_preds = None
 
     #Property
     @property
