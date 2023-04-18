09bb19a3275cdf9932d27ef6606f7815c1f7bdc4
ReapD4ni <115945147+ReapD4ni@users.noreply.github.com>
2023-04-18 11:20:50 +0200

    Update LinearRegressionSkeleton.py

--- GYAK/GYAK08/LinearRegressionSkeleton.py
+++ GYAK/GYAK08/LinearRegressionSkeleton.py
@@ -1,56 +1,40 @@
-import pandas as pd
 import numpy as np
-from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-
-from matplotlib import pyplot as plt
 
 
 class LinearRegression:
     def __init__(self, epochs: int = 1000, lr: float = 1e-3):
-        self.epochs=epochs
-        self.lr=lr
-
-
-    iris = load_iris()
-    df = pd.DataFrame(iris.data, columns=iris.feature_names)
-
-    X = df['petal width (cm)'].values
-    y = df['sepal length (cm)'].values
+        self.epochs = epochs
+        self.lr = lr
 
     def fit(self, X: np.array, y: np.array):
 
-        
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.m = 0
+        self.c = 0
 
-        # Building the model
-        m = 0
-        c = 0
+        n = float(len(X)) 
 
-        L = 0.0001  # The learning Rate
-        epochs = 1000  # The number of iterations to perform gradient descent
-
-        n = float(len(self.X_train)) # Number of elements in X
-
-        # Performing Gradient Descent 
         losses = []
-        for i in range(epochs): 
-            y_pred = m*self.X_train + c  # The current predicted value of Y
 
-            residuals = self.y_pred - self.y_train
+        for i in range(self.epochs):
+            self.y_pred = self.m * X + self.c
+
+            residuals = self.y_pred - y
             loss = np.sum(residuals ** 2)
             losses.append(loss)
-            D_m = (-2/n) * sum(self.X_train * residuals)  # Derivative wrt m
-            D_c = (-2/n) * sum(residuals)  # Derivative wrt c
-            m = m + L * D_m  # Update m
-            c = c + L * D_c  # Update c
+            D_m = (-2/n) * sum(X * residuals)
+            D_c = (-2/n) * sum(residuals)  
+            self.m = self.m + self.lr * D_m  
+            self.c = self.c + self.lr * D_c  
             if i % 100 == 0:
-                print(np.mean(self.y_train-self.y_pred))
+                print(np.mean(y-self.y_pred))
+
+
 
 
     def predict(self, X):
         pred = []
-        for X in self.X_test:
-            y_pred = m*self.X + c
+        for x in X:
+            y_pred = self.m*x + self.c
             pred.append(y_pred)
+        print(pred)
